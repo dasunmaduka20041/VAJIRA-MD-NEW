f135f245fdea87be13dc797f33bbef4e6f794dc3
dasunmaduka20041 <144513258+dasunmaduka20041@users.noreply.github.com>
2024-10-02 21:21:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94773620112' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zFkATQjQ#GV2pUQIQWCbpvCPiVAoT9fcZ5OzUmY8jHxrQAbFc_6k' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zFkATQjQ#VAJIRA-MD=6FEjECyQ#otP3aUBs9OoR36kjyilZLc1LZHA_fq4VWjNo_Ad82NI' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
